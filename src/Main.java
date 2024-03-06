8dc242b20d84f7f00f21e4f1ce12daae0387c56d
derossca <derossca@mail.uc.edu>
2024-03-06 13:58:34 -0500

    imported random

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+//importing random
+import java.util.Random;
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        Random rnd = new Random();
+
     }
 }
