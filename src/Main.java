e4a01bc3f96c681330afb520f667dc9440f66a62
derossca <derossca@mail.uc.edu>
2024-03-18 10:04:44 -0400

    debugging and updating

--- src/Main.java
+++ src/Main.java
@@ -59,6 +59,5 @@ public class Main {
         //using static method for userNum to get input of an integer within range set
         int userNum = SafeInput.getRangedInt(in, "Choose a number between 1 - 100", 1, 100);
 
-
     }
 }
