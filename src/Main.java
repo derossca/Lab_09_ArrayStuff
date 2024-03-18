0c18d897233655b5b559a502ecdcfbf53b338cc7
derossca <derossca@mail.uc.edu>
2024-03-18 09:37:54 -0400

    completed part d of Task 1, did some debugging and optimizing

--- src/Main.java
+++ src/Main.java
@@ -28,19 +28,32 @@ public class Main {
             //gives 100 integers at randomn from 1 - 100
             dataPoints[row] = rnd.nextInt(100) + 1;
             //displays dataPoints on same line separated by an |
-            System.out.printf(dataPoints[row] + " | ");
+            System.out.print(dataPoints[row] + " | ");
         }
 
         //to print what's next on new line
         System.out.println();
 
         //part d
+        //declaring and initializing variable for sum of dataPoints
+        int sumDataPoints = 0;
+        double aveDataPoints = 0;
         //for loop to display array
         for(int row = 0; row < dataPoints.length; row ++)
         {
-            //gives 100 random integers from 1 - 100
+            //creates 100 random integers between 1 - 100
             dataPoints[row] = rnd.nextInt(100) + 1;
-            System.out.println(dataPoints[row]);
+            //gives the sum of all the data points added up
+            sumDataPoints = sumDataPoints + dataPoints[row];
         }
+        //prints out the sum
+        System.out.printf("The sum of all the data points is " + sumDataPoints);
+        System.out.println(); // new line
+        aveDataPoints = sumDataPoints / dataPoints.length; //gives the average of all the data points
+        System.out.printf("The average of all the data points is " + aveDataPoints);
+
+        //TASK 2
+
+
     }
 }
