47ecee496d38de722450bf43070a3669e81661cb
derossca <derossca@mail.uc.edu>
2024-03-18 08:28:49 -0400

    declared array, determined its index, did part a, b, and c of task 1

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,42 @@ public class Main {
     {
         Random rnd = new Random();
 
+        //TASK 1
+        //part a
+        //declare
+        int [] dataPoints;
+        //size
+        dataPoints = new int[101];
+
+        //part b
+        //for loop to display array
+        for(int row = 0; row < dataPoints.length; row ++)
+        {
+            //gives 100 integers at random from 1 - 100
+            dataPoints[row] = rnd.nextInt(100) + 1;
+            System.out.println(dataPoints[row]);
+        }
+
+        //part c
+        //for loop to display array
+        for(int row = 0; row < dataPoints.length; row ++)
+        {
+            //gives 100 integers at randomn from 1 - 100
+            dataPoints[row] = rnd.nextInt(100) + 1;
+            //displays dataPoints on same line separated by an |
+            System.out.printf(dataPoints[row] + " | ");
+        }
+
+        //to print what's next on new line
+        System.out.println();
+
+        //part d
+        //for loop to display array
+        for(int row = 0; row < dataPoints.length; row ++)
+        {
+            //gives 100 random integers from 1 - 100
+            dataPoints[row] = rnd.nextInt(100) + 1;
+            System.out.println(dataPoints[row]);
+        }
     }
 }
