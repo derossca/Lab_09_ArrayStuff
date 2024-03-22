37086c66ee3d37c0da3686384c99d371d7f1e836
derossca <derossca@mail.uc.edu>
2024-03-22 08:52:02 -0400

    added java class for doing extra credit

---
+++ src/ExtraCredit.java
@@ -0,0 +1,13 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class ExtraCredit {
+    public static void main(String[] args) {
+        Random rnd = new Random();
+        Scanner in = new Scanner(System.in);
+
+        //declaring and initializing array
+
+        int[] dataPoints = new int[100];
+    }
+}
