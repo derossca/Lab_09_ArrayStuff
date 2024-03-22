b216ba736061fa810d4fe3e79c4cd7f34864745e
derossca <derossca@mail.uc.edu>
2024-03-22 09:15:23 -0400

    created static method for min

--- src/ExtraCredit.java
+++ src/ExtraCredit.java
@@ -1,3 +1,4 @@
+//importing random and scanner
 import java.util.Random;
 import java.util.Scanner;
 
@@ -9,5 +10,39 @@ public class ExtraCredit {
         //declaring and initializing array
 
         int[] dataPoints = new int[100];
+
+        //
+        System.out.println("The min of dataPoints is: " + min(dataPoints));
+    }
+
+    public static int min(int values[])
+    {
+        //declaring and initializing random
+        Random rnd = new Random();
+        //declare and initialize array
+        int [] dataPoints = new int[100];
+        //declaring variables
+        int min = 0;
+
+        for(int row = 0; row < dataPoints.length; row ++)
+        {
+            //gives 100 integers at random from 1 - 100
+            dataPoints[row] = rnd.nextInt(100) + 1;
+
+            //if condition to find min value
+            if(min > dataPoints[row])
+            {
+                min = dataPoints[row];
+            }
+            //printing table to see the random numbers generated for min/max
+            System.out.printf("%5d",dataPoints[row]);
+            if (row % 20 == 0)
+            {
+                System.out.println("");
+            }
+        }
+
+        System.out.println();
+        return min;
     }
 }
